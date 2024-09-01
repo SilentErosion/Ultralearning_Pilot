4e31ea1152c7b6355d7558cb0f75e69868695974
SilentErosion <141993381+SilentErosion@users.noreply.github.com>
2024-09-01 16:00:38 +0900

    Refactored version of string calculators
    Reduced the responsibility of certain methods.
    Made a function for the conditional statement: This is recommended due to readability and reusability.
    Exception type changes: Throwable to Exception class, IllegalArgumentException to RuntimeException.

--- Java 웹 프로그래밍 Next Step/StringCalc.java
+++ Java 웹 프로그래밍 Next Step/StringCalc.java
@@ -1,52 +1,62 @@
 package StringCalc;
 
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalc {
-	public int add(String input) throws Throwable {
-		if(input == null || input.isEmpty()) {
+	public int add(String input) {
+		if (isEmpty(input)) {
 			return 0;
 		}
-		
-		int[] nums = tokenize(input);
-		int total = calcTotal(nums);
+
+		String[] intStrings = tokenize(input);
+		int[] posInts = toPosInts(intStrings);
+		int total = calcTotal(posInts);
+
 		return total;
 	}
-	
-	private int[] tokenize(String input) {
-		String[] intString = null;
-		int[] intArray = null;
-		
+
+	private String[] tokenize(String input) {
+		String[] strings = null;
+
 		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
 		// Custom Delimiter
 		if (m.find()) {
 			String customDelimeter = m.group(1);
-			intString = m.group(2).split(Pattern.quote(customDelimeter));
+			strings = m.group(2).split(Pattern.quote(customDelimeter));
 		}
 		// Formal Delimiter
 		else {
-			intString = input.split(",|:");
+			strings = input.split(",|:");
 		}
-		
-		intArray = new int[intString.length];
-		for (int i = 0; i < intArray.length; ++i) {
-			intArray[i] = Integer.parseInt(intString[i]);
-		}
-		return intArray;
+
+		return strings;
 	}
-	
+
 	private int calcTotal(int[] nums) {
 		int sum = 0;
-		for(int num : nums) {
-			if(num < 0) throw new IllegalArgumentException();
+		for (int num : nums) {
 			sum += num;
 		}
 		return sum;
 	}
-	
+
+	private boolean isEmpty(String str) {
+		return str == null || str.isEmpty();
+	}
+
+	private int[] toPosInts(String[] strings) {
+		int[] posInts = new int[strings.length];
+		for (int i = 0; i < posInts.length; ++i) {
+			posInts[i] = Integer.parseInt(strings[i]);
+			if (posInts[i] < 0) {
+				throw new RuntimeException();
+			}
+		}
+		return posInts;
+	}
+
 	public static void main(String[] args) {
-		
-	}	
+
+	}
 }
--- Java 웹 프로그래밍 Next Step/StringCalcTest.java
+++ Java 웹 프로그래밍 Next Step/StringCalcTest.java
@@ -2,53 +2,54 @@ package StringCalc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class StringCalcTest {
-	private StringCalc hw;
+	private StringCalc strCalc;
+
 	@BeforeEach
 	public void setup() {
-		hw = new StringCalc();
+		strCalc = new StringCalc();
 	}
-	
+
 	// null이나 empty string
 	@ParameterizedTest
 	@NullSource
-	@ValueSource(strings = {""})
-	void nullAndEmptyTest(String input) throws Throwable {
-		assertEquals(0, hw.add(input));
+	@ValueSource(strings = { "" })
+	void nullAndEmptyTest(String input) throws Exception {
+		assertEquals(0, strCalc.add(input));
 	}
-	
+
 	// 숫자 1개면 그대로
 	@ParameterizedTest
-	@ValueSource(strings = {"3"})
-	void oneNumTest(String input) throws Throwable {
-		assertEquals(3, hw.add(input));
+	@ValueSource(strings = { "3" })
+	void oneNumTest(String input) throws Exception {
+		assertEquals(3, strCalc.add(input));
 	}
-	
+
 	// 구분자를 쉼표나 콜론
 	@ParameterizedTest
-	@ValueSource(strings = {"1:2:3", "2,3,1", "2,3:1"})
-	void formalDelimeterTest(String input) throws Throwable {
-		assertEquals(6, hw.add(input));
+	@ValueSource(strings = { "1:2:3", "2,3,1", "2,3:1" })
+	void formalDelimeterTest(String input) throws Exception {
+		assertEquals(6, strCalc.add(input));
 	}
-	
+
 	// 커스텀 구분자
 	@ParameterizedTest
-	@ValueSource(strings = {"//.\n1.2.3"})
-	void customDelimeterTest(String input) throws Throwable {
-		assertEquals(6, hw.add(input));
+	@ValueSource(strings = { "//.\n1.2.3" })
+	void customDelimeterTest(String input) throws Exception {
+		assertEquals(6, strCalc.add(input));
 	}
-	
+
 	// 음수가 있는 경우
 	@ParameterizedTest
-	@ValueSource(strings = {"-1,3,2"})
+	@ValueSource(strings = { "1,3,-2" })
 	void negNumTest(String input) {
-		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            hw.add(input);
-        });
+		assertThrows(RuntimeException.class, () -> {
+			strCalc.add(input);
+		});
 	}
-	
+
 }
